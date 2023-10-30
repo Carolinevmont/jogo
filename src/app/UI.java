07462c3739ef0d8addbb9c3e9a9e77d4decd6742
Carolinevmont <carolinemonteirov@gmail.com>
2023-10-30 11:21:22 -0300

    atualização

--- src/app/UI.java
+++ src/app/UI.java
@@ -3,6 +3,7 @@ package app;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -46,6 +47,14 @@ public class UI {
 		catch (RuntimeException e) {
 			throw new InputMismatchException("erro lendo posição de xadrez.");
 		}	
+	}
+	
+	public static void printMatch(ChessMatch chessMatch) {
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.println("vez: " + chessMatch.getTurn());
+		System.out.println("esperando jogador jogar: " + chessMatch.getCurrentPlayer());
+		
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces) {
